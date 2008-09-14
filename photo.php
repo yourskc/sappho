d05424ceb3acab5f36c4ace8ed58e9063dcf2594
lucasreddinger <lucasreddinger@99f8f9f2-4d54-0410-9982-1d7539c1c236>
2008-09-14 16:47:16 +0000

    resolved issue 6.

--- photo.php
+++ photo.php
@@ -16,6 +16,8 @@ $image_id = clean($_GET['id']);
 $sql = "SELECT filename,                    ".
        "       title,                       ".
        "       caption,                     ".
+       "       sort,                        ".
+       "       set_id,                      ".
        "       exif_cameramodel,            ".
        "       exif_exposuretime,           ".
        "       exif_fnumber,                ".
@@ -56,7 +58,21 @@ echo "            </div>\n";
 
 echo "            <div id=\"caption\">".nl2br($image["caption"])."</div>\n";
 
-echo "            <div id=\"link_next\"><a href=\"about:blank\">view next in set</a></div>\n";
+
+$sql = "SELECT image_id                     ".
+       "FROM photo_image                    ".
+       "WHERE set_id='{$image["set_id"]}'   ".
+       "   && sort > {$image["sort"]}       ".
+       "ORDER BY sort                       ".
+       "LIMIT 1                             ";
+if (!$result = mysql_query($sql)) print_error();
+
+if (mysql_num_rows($result) === 1) {
+    
+    $next_img = mysql_fetch_array($result);
+    echo "            <div id=\"link_next\"><a href=\"$sappho_path/photo/{$next_img["image_id"]}/\">view next in set</a></div>\n";
+
+};
 
 echo "            <div id=\"link_full\"><a href=\"http://$s3_bucket.s3.amazonaws.com/$s3_path/a/{$image["filename"]}.jpg\">view in high resolution</a></div>\n";
 

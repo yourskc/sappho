fd63abe2f12004b56c4bbeaac9f197c089ff1735
lucasreddinger <lucasreddinger@99f8f9f2-4d54-0410-9982-1d7539c1c236>
2008-10-09 08:21:13 +0000

    let's use htmlentities() instead of htmlspecialchars() for robustness. [issue 12]

--- trunk/global.php
+++ trunk/global.php
@@ -92,7 +92,7 @@ function clean($text) {
  *****************/
 function output($text) {
     global $entry_find, $entry_replace;
-    $text = htmlspecialchars($text, ENT_QUOTES, "ISO-8859-1", FALSE);
+    $text = htmlentities($text, ENT_QUOTES, "ISO-8859-1", FALSE);
     $text = str_replace($entry_find, $entry_replace, $text);
     $text = nl2br($text);
     return $text;
